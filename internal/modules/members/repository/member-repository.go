9e01bc7b940979f15b769213dbf5f0dbbecc784e
DinizJ <rodrigocljr07@gmail.com>
2026-02-08 15:54:52 -0300

    Better organization

--- internal/modules/members/repository/member-repository.go
+++ internal/modules/members/repository/member-repository.go
@@ -16,47 +16,7 @@ func NewMemberRepository(db *sql.DB) *MemberRepository {
 	return &MemberRepository{}
 }
 
-func (r *MemberRepository) Save(ctx context.Context, member *model.Member) error {
-	query := `
-		INSERT INTO members (id, nome, nome_religioso, cpf, rg, data_nascimento, sexo, telefone, email,
-		endereco_rua, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado,
-		endereco_cep, cargo, status, odun, observacoes, created_at, updated_at) 
-		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-		`
-
-	_, err := r.db.ExecContext(ctx, query,
-		member.ID,
-		member.NomeCompleto,
-		member.NomeReligioso,
-		member.CPF,
-		member.RG,
-		member.DataNascimento,
-		member.Sexo,
-		member.Telefone,
-		member.Email,
-
-		member.Endereco.Rua,
-		member.Endereco.Numero,
-		member.Endereco.Complemento,
-		member.Endereco.Bairro,
-		member.Endereco.Cidade,
-		member.Endereco.Estado,
-		member.Endereco.CEP,
-
-		member.Cargo,
-		member.Status,
-		member.DataIniciacao,
-		member.Observacoes,
-		member.CreatedAt,
-		member.UpdatedAt)
-
-	if err != nil {
-		return fmt.Errorf("Failed to save member: %w", err)
-	}
-
-	return nil
-}
-
+// Scan
 func scanMember(rows *sql.Rows) (*model.Member, error) {
 	var member model.Member
 
@@ -125,4 +85,52 @@ func scanMemberRow(row *sql.Row) (*model.Member, error) {
 		&member.UpdatedAt,
 		&member.DeletedAt,
 	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &member, nil
+}
+
+//Save new member
+func (r *MemberRepository) Save(ctx context.Context, member *model.Member) error {
+	query := `
+		INSERT INTO members (id, nome, nome_religioso, cpf, rg, data_nascimento, sexo, telefone, email,
+		endereco_rua, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado,
+		endereco_cep, cargo, status, odun, observacoes, created_at, updated_at) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		`
+
+	_, err := r.db.ExecContext(ctx, query,
+		member.ID,
+		member.NomeCompleto,
+		member.NomeReligioso,
+		member.CPF,
+		member.RG,
+		member.DataNascimento,
+		member.Sexo,
+		member.Telefone,
+		member.Email,
+
+		member.Endereco.Rua,
+		member.Endereco.Numero,
+		member.Endereco.Complemento,
+		member.Endereco.Bairro,
+		member.Endereco.Cidade,
+		member.Endereco.Estado,
+		member.Endereco.CEP,
+
+		member.Cargo,
+		member.Status,
+		member.DataIniciacao,
+		member.Observacoes,
+		member.CreatedAt,
+		member.UpdatedAt)
+
+	if err != nil {
+		return fmt.Errorf("Failed to save member: %w", err)
+	}
+
+	return nil
 }
