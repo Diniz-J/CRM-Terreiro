7b1194fb357a28f96c639ebfea437c5ef4a5462d
DinizJ <rodrigocljr07@gmail.com>
2026-02-08 18:42:58 -0300

    SearchbyName repo added

--- internal/modules/members/repository/member-repository.go
+++ internal/modules/members/repository/member-repository.go
@@ -20,6 +20,8 @@ func NewMemberRepository(db *sql.DB) *MemberRepository {
 func scanMember(rows *sql.Rows) (*model.Member, error) {
 	var member model.Member
 
+	member.Endereco = model.Endereco{} //Garantia de não ser nil
+
 	err := rows.Scan(
 		&member.ID,
 		&member.NomeCompleto,
@@ -154,4 +156,31 @@ func (r *MemberRepository) FindByID(ctx context.Context, id string) (*model.Memb
 		return nil, fmt.Errorf("Failed to find member by id(%s): %w", id, err)
 	}
 	return member, nil
+}
+
+func (r *MemberRepository) SearchByName(ctx context.Context, nome string) ([]*model.Member, error) {
+	query := `
+		SELECT id, nome, nome_religioso, cpf, rg, data_nascimento, sexo, telefone, email,
+		endereco_rua, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado,
+		endereco_cep, cargo, status, odun, observacoes, created_at, updated_at, deleted_at   FROM members 
+		WHERE nome LIKE ? AND deleted_at IS NULL`
+
+	search := "%" + nome + "%"
+
+	rows, err := r.db.QueryContext(ctx, query, search)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to search members: %w", err)
+	}
+	defer rows.Close()
+
+	var members []*model.Member
+
+	for rows.Next() {
+		member, err := scanMember(rows)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to search members: %w", err)
+		}
+		members = append(members, member)
+	}
+	return members, nil
 }
