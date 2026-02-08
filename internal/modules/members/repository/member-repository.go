2fe3de844b9a18f25fde5fbb6b5ea6883acc59f5
DinizJ <rodrigocljr07@gmail.com>
2026-02-08 15:24:19 -0300

    Add repo save

--- internal/modules/members/repository/member-repository.go
+++ internal/modules/members/repository/member-repository.go
@@ -1,1 +1,58 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/Diniz-J/teiunecc-admin/internal/modules/members/model"
+)
+
+type MemberRepository struct {
+	db *sql.DB
+}
+
+func NewMemberRepository(db *sql.DB) *MemberRepository {
+	return &MemberRepository{}
+}
+
+func (r *MemberRepository) Save(ctx context.Context, member *model.Member) error {
+	query := `
+		INSERT INTO members (id, nome, nome_religioso, cpf, rg, data_nascimento, sexo, telefone, email,
+		endereco_rua, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade, endereco_estado,
+		endereco_cep, cargo, status, odun, observacoes, created_at, updated_at) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		`
+
+	_, err := r.db.ExecContext(ctx, query,
+		member.ID,
+		member.NomeCompleto,
+		member.NomeReligioso,
+		member.CPF,
+		member.RG,
+		member.DataNascimento,
+		member.Sexo,
+		member.Telefone,
+		member.Email,
+
+		member.Endereco.Rua,
+		member.Endereco.Numero,
+		member.Endereco.Complemento,
+		member.Endereco.Bairro,
+		member.Endereco.Cidade,
+		member.Endereco.Estado,
+		member.Endereco.CEP,
+
+		member.Cargo,
+		member.Status,
+		member.DataIniciacao,
+		member.Observacoes,
+		member.CreatedAt,
+		member.UpdatedAt)
+
+	if err != nil {
+		return fmt.Errorf("Failed to save member: %w", err)
+	}
+
+	return nil
+}
