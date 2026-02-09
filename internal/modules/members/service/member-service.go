10289f6a457d80b887cd41225173395ab33bd38c
DinizJ <rodrigocljr07@gmail.com>
2026-02-08 22:58:25 -0300

    service base

--- internal/modules/members/service/member-service.go
+++ internal/modules/members/service/member-service.go
@@ -1,3 +1,13 @@
 package service
 
-func
+import (
+	"github.com/Diniz-J/teiunecc-admin/internal/modules/members/repository"
+)
+
+type MemberService struct {
+	repo *repository.MemberRepository
+}
+
+func NewMemberService(repo *repository.MemberRepository) *MemberService {
+	return &MemberService{repo: repo}
+}
