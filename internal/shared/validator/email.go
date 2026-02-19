ad305234bdc678a15112cef62d31614bc92c2df0
Rodrigo Diniz <rodrigocljr07@gmail.com>
2026-02-18 21:23:49 -0300

    add email validator

---
+++ internal/shared/validator/email.go
@@ -0,0 +1,9 @@
+package shared
+
+import "regexp"
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+func Email(email string) bool {
+	return len(email) <= 254 && emailRegex.MatchString(email)
+}
