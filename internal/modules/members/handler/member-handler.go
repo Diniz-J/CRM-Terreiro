1435d203a41ae6e67190da42c12f1c5368629c94
Rodrigo Diniz <rodrigocljr07@gmail.com>
2026-03-10 14:52:09 -0300

    feat: add update handler

--- internal/modules/members/handler/member-handler.go
+++ internal/modules/members/handler/member-handler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Diniz-J/teiunecc-admin/internal/modules/members/service"
 	"github.com/Diniz-J/teiunecc-admin/internal/shared/response"
+	"github.com/gorilla/mux"
 )
 
 type MemberHandler struct {
@@ -15,6 +16,16 @@ type MemberHandler struct {
 
 func NewMemberHandler(service *service.MemberService) *MemberHandler {
 	return &MemberHandler{service: service}
+}
+
+func (h *MemberHandler) handleServiceError(w http.ResponseWriter, err error) {
+	if errors.Is(err, service.ErrInvalidCPF) ||
+		errors.Is(err, service.ErrInvalidEmail) ||
+		errors.Is(err, service.ErrInvalidPhone) {
+		response.Error(w, http.StatusBadRequest, "BAD_REQUEST", err.Error())
+		return
+	}
+	response.Error(w, http.StatusInternalServerError, "INTERNAL_ERROR", err.Error())
 }
 
 func (h *MemberHandler) CreateMember(w http.ResponseWriter, r *http.Request) {
@@ -26,13 +37,26 @@ func (h *MemberHandler) CreateMember(w http.ResponseWriter, r *http.Request) {
 
 	member, err := h.service.CreateMember(r.Context(), input)
 	if err != nil {
-		if errors.Is(err, service.ErrInvalidCPF) ||
-			errors.Is(err, service.ErrInvalidEmail) ||
-			errors.Is(err, service.ErrInvalidPhone) {
-			response.Error(w, http.StatusBadRequest, "BAD_REQUEST", err.Error())
-			return
-		}
-		response.Error(w, http.StatusInternalServerError, "INTERNAL_ERROR", err.Error())
+		h.handleServiceError(w, err)
+		return
+	}
+
+	response.JSON(w, http.StatusCreated, member)
+}
+
+func (h *MemberHandler) UpdateMember(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var input service.MemberInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		response.Error(w, http.StatusBadRequest, "BAD_REQUEST", "invalid body")
+		return
+	}
+
+	member, err := h.service.UpdateMember(r.Context(), id, input)
+	if err != nil {
+		h.handleServiceError(w, err)
 		return
 	}
 
