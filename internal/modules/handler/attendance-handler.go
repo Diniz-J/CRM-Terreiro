08ac8ea196a6956ce65136516ab855811d80d51f
Rodrigo Diniz <rodrigocljr07@gmail.com>
2026-03-29 02:44:34 -0300

    feat: add markattendance handler

---
+++ internal/modules/handler/attendance-handler.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"errors"
+
+	"github.com/Diniz-J/teiunecc-admin/internal/modules/service"
+	"github.com/gofiber/fiber/v2"
+)
+
+type AttendanceHandler struct {
+	service *service.AttendanceService
+}
+
+func NewAttendanceHandler(service *service.AttendanceService) *AttendanceHandler {
+	return &AttendanceHandler{service: service}
+}
+
+func (h *AttendanceHandler) handleAttendanceError(c *fiber.Ctx, err error) error {
+	if errors.Is(err, service.ErrAttendanceNotFound) {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": fiber.Map{
+				"code":    "NOT_FOUND",
+				"message": err.Error(),
+			},
+		})
+	}
+	if errors.Is(err, service.ErrMissingRequirement) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": fiber.Map{
+				"code":    "BAD_REQUEST",
+				"message": err.Error(),
+			},
+		})
+	}
+	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"error": fiber.Map{
+			"code":    "INTERNAL_ERROR",
+			"message": "internal server error",
+		},
+	})
+
+}
+
+func (h *AttendanceHandler) MarkAttendance(c *fiber.Ctx) error {
+	var input service.AttendanceInput
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": fiber.Map{
+				"code":    "BAD_REQUEST",
+				"message": err.Error(),
+			},
+		})
+	}
+
+	attendance, err := h.service.MarkAttendance(c.Context(), input)
+	if err != nil {
+		return h.handleAttendanceError(c, err)
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(attendance)
+}
