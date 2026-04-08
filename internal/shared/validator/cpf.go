c9e3c9c0fe988cc66a6a7e275677949dffdb323b
Rodrigo Diniz <rodrigocljr07@gmail.com>
2026-04-08 09:14:17 -0300

    fix: implementar validação completa de CPF com algoritmo módulo 11

--- internal/shared/validator/cpf.go
+++ internal/shared/validator/cpf.go
@@ -5,24 +5,19 @@ import (
 	"strings"
 )
 
-//^começo // $fim
-
-// \d numero \w letra ou numero \s espaço . qq caracteres
-
-// \D!numero \W!letra/numero \S!espaço
-
-var cpfRegex = regexp.MustCompile(`^(\d{11})$`)
+var cpfRegex = regexp.MustCompile(`^\d{11}$`)
 
 func CPF(cpf string) bool {
 	cpf = strings.ReplaceAll(cpf, ".", "")
 	cpf = strings.ReplaceAll(cpf, "-", "")
 
-	if len(cpf) != 11 {
+	if !cpfRegex.MatchString(cpf) {
 		return false
 	}
 
+	// rejeita sequências como 00000000000, 11111111111, etc.
 	allSame := true
-	for i := 0; i < 11; i++ {
+	for i := 1; i < 11; i++ {
 		if cpf[i] != cpf[0] {
 			allSame = false
 			break
@@ -32,5 +27,29 @@ func CPF(cpf string) bool {
 		return false
 	}
 
-	return cpfRegex.MatchString(cpf)
+	// valida 1º dígito verificador
+	sum := 0
+	for i := 0; i < 9; i++ {
+		sum += int(cpf[i]-'0') * (10 - i)
+	}
+	resto := sum % 11
+	primeiro := 0
+	if resto >= 2 {
+		primeiro = 11 - resto
+	}
+	if int(cpf[9]-'0') != primeiro {
+		return false
+	}
+
+	// valida 2º dígito verificador
+	sum = 0
+	for i := 0; i < 10; i++ {
+		sum += int(cpf[i]-'0') * (11 - i)
+	}
+	resto = sum % 11
+	segundo := 0
+	if resto >= 2 {
+		segundo = 11 - resto
+	}
+	return int(cpf[10]-'0') == segundo
 }
